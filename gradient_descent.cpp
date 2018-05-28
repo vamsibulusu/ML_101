5eb4d6083d3886d46a3e12610b6a59e4ed1eaa30
Vamsi Bulusu <vamsib123@gmail.com>
2018-05-28 14:42:33 +0530

    Update gradient_descent.cpp

--- gradient_descent.cpp
+++ gradient_descent.cpp
@@ -1,141 +1,62 @@
-#include "gradient_descent.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "gradient.h"
 
-#include<bits/stdc++.h>
 using namespace std;
 
-
-
-long int dataset::no_rows()
+ostream& operator<<(ostream& os, TrainingExample& te)
 {
-return r_data.size();
+    int n = te.getFeatures().size();
+    os << "(";
+    for (int i = 0; i < n; i++)
+        os << te.getFeature(i) << (i != n-1 ? ", " : ""); 
+    os << ")";
+    return os;
 }
 
-void dataset::populate_data(string location,long int no_rows,long int no_columns)
+int main()
 {
+    int M = 0, N = 0;
+    vector<TrainingExample> ts;
+    ifstream f;
+    f.open("data.txt");
+    if (!f.is_open())
+    {
+        cout << "File not opened" << endl;
+        return 1;
+    }
+    f >> M >> N;
+    cout << "M = " << M << ", N = " << N << endl;
+    for (int i = 0; i < M; i++)
+    {
+        vector<int> feat(N+1, 1);
+        int y = 0;
+        for (int j = 0; j < N; j++)
+            f >> feat[j+1];
+        f >> y;
 
-ifstream fin;
-fin.open("forest_fires.txt");
-if(fin.is_open())
-{
-long int count_c=0;
-long int count_r=0;
-string str_no;
-vector<double> tempe;
-double temp;
-while(!fin.eof())
-{
+        TrainingExample te(feat, y);
+        ts.push_back(te);
+    }
+    f.close();
 
+    for (unsigned i = 0; i < ts.size(); i++)
+        cout << "Example " << i << ": " << ts[i] << endl;
 
-if(count_c!=no_columns-1)
-{
-getline(fin,str_no,',');
-stringstream converter(str_no);
-converter >> temp;
+    Hypothesis hyp(ts);
+    vector<double> theta = hyp.gradientDescent();
 
-tempe.push_back(temp);
-count_c=count_c+1;
+    cout << endl;
+    for (size_t i = 0; i < theta.size(); i++)
+        cout << "th" << i << " = " << theta[i] << " ";
+    cout << endl;
 
+    cout << "Input x1 x2" << endl;
+    int x1, x2;
+    cin >> x1 >> x2;
+    cout << "H = " << (theta[0]+theta[1]*x1+theta[2]*x2) << endl;
+    
+
+    return 0;
 }
-if(count_c==no_columns-1)
-{
-
-getline(fin,str_no,'\n');
-stringstream converter(str_no);
-converter >> temp;
-r_data.push_back(temp);
-f_data.push_back(tempe);
-for(int i=0;i<tempe.size();i++){cout<<tempe[i]<<" ";}
-tempe.clear();
-count_r++;
-count_c=0;
-
-}
-}
-}
-else
-{
-cout<<"could not open the file!";
-}
-}
-
-void hypothesis::cost_calculation(dataset& data,long int rows)
-{
-
-for(int i=0;i<rows;i++)
-{
-
-h_thetha.push_back(inner_product(thetha.begin(),thetha.end(),data.f_data[i].begin(),0));
-cout<<h_thetha[i];
-}
-
-//return h_thetha;
-}
-
-void hypothesis::check(double epsilon)
-{
-//cout<<abs(jn1-jn2);
-if(abs(jn1-jn2)<=epsilon)
-{
-check_flag=true;
-}
-}
-
-
-
-
-
-
-
-vector<double> hypothesis::grad_descent(double alpha,double eps, dataset& data,long int row,long int col)
-{
-
-for(long int i=0;i<data.r_data.size();i++)
-{
-thetha.push_back(0);
-}
-jn1=0;jn2=10000001;
-check_flag=false;
-
-//vector<double> h_thetha;
-
-hypothesis::cost_calculation(data,row);
-//vector<double> j_thetha;
-cout<<"DDDDDDDDDDDD";
-std::transform(h_thetha.begin(),h_thetha.end(),data.r_data.begin(),j_thetha.begin(),std::minus<int>());
-vector<double> inter(j_thetha);
-std::transform(j_thetha.begin(),j_thetha.end(),j_thetha.begin(), j_thetha.begin(), multiplies<double>());
-jn1=std::accumulate(j_thetha.begin(), j_thetha.end(), 0);
-jn1=jn1/(2*data.f_data.size());
-
-
-
-
-check_flag=false;
-while(check_flag==false)
-{jn1=jn2;
-
-for(long int i=0;i<thetha.size();i++)
-{
-double sum=0;
-for(long int j=0;j<data.f_data.size();j++)
-{
-sum=sum+(inter[j]*data.f_data[j][i]);
-}
-thetha[i]=thetha[i]-(alpha*(sum));
-}
-
-h_thetha.clear();
-hypothesis::cost_calculation(data,row);
-j_thetha.clear();
-std::transform(h_thetha.begin(),h_thetha.end(),data.r_data.begin(),j_thetha.begin(),std::minus<int>());
-inter.clear();
-copy(j_thetha.begin(),j_thetha.end(),inter.begin());
-//inter(j_thetha);
-std::transform(j_thetha.begin(),j_thetha.end(),j_thetha.begin(), j_thetha.begin(), multiplies<double>());
-jn2=std::accumulate(j_thetha.begin(), j_thetha.end(), 0);
-jn2=jn2/(2*data.f_data.size());
-check(eps);
-}
-return thetha;
-}
-
